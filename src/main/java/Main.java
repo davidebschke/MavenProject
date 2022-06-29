83cc45e8a8f9779c7f060d541b4c631c5c6975f0
davidebschke <Davidebschke8@gmail.com>
2022-06-29 17:08:53 +0200

    Java / Maven Test 2nd
    New:
    
    - countToTen Method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hallo Welt");
-        int i = 0;
-        while (i < 11) {
-            System.out.println("Die aktuelle Zahl ist" + i);
-            i++;
+        countToTen();
+
+        }
+
+    public static void countToTen() {
+        int number=0;
+        while (number < 11) {
+            System.out.println("Die aktuelle Zahl ist " + number);
+            number++;
         }
     }
 
 }
+
+
