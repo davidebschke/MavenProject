d92d6e40824a446a57d448feea4251b0863a80fe
davidebschke <Davidebschke8@gmail.com>
2022-06-29 17:04:07 +0200

    Java / Maven Test 2nd
    New:
    
    - count with while

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hallo Welt");
+        int i = 0;
+        while (i < 11) {
+            System.out.println("Die aktuelle Zahl ist" + i);
+            i++;
+        }
     }
+
 }
